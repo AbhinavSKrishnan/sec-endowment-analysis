2ebd8908e9e09c422b85b57c2d9ef820fe8e86a6
Abhinav Krishnan <abhinavskrishnan@Abhinavs-MacBook-Pro-2.local>
2022-11-12 13:33:33 -0600

    I need to commit more often lol 1. created for loop to generate filing urls from cik numbers and asccension numbers 2. created for loop to create infotable urls using filing urls (this needs to be updated to use the filing table instead) 3. created for loop to read, format, and stack infotable information into a list of entities
    functionally, this code achieves everything I need it to! Now, I want to fix the issues that are causing bugs and optimize the output

--- 10_scrape_data.R
+++ 10_scrape_data.R
@@ -13,6 +13,20 @@
 
     
 
+# =============================================================================
+# ================================ Status =====================================
+# =============================================================================
+
+  # 11 November - Completed csv entry and reading; produced urls to 
+
+
+# =============================================================================
+# =============================== Notes ====================================
+# =============================================================================
+
+  # WAIT: I'm assuming that all 13F filings title their infotable the same way. This assumption might not hold
+  # we'll deal with that in a bit
+
 # ================= STEPS ================= 
 # 1. scrape stock table from information table
 # 2. automate opening information table from filing page
@@ -26,7 +40,9 @@
     # Form 13F Information Table (Click on html information table)
     # Xpath: /html/body/table[2]/tbody
 
-# SETUP
+# =============================================================================
+# =============================== Setup =====================================
+# =============================================================================
 
   # clear working environment
     rm(list = ls())
@@ -62,166 +78,214 @@
     p_url_base <- "https://www.sec.gov"
     p_url_data <- paste0(p_url_base, "/Archives/edgar/data")
 
-# ================ STAGE ONE: Accension Number Extraction =====================
+# =============================================================================
+# ========================= Filing Table Processing ==============================
+# =============================================================================
+
+    # Unfortunately, I was unable to extract the filing tables from the entity pages automatically, so I've downloaded a
+    # csv version of each filing table manually.
+    # In the future, this step could be automated by using RSelenium or revisiting the html code
     
-    # for the purposes of this project, I am okay with downloading each csv of filings manually
     # I can take the ascension numbers from that data table and continue with the other steps of this process!
     
-    # WAIT: I'm assuming that all 13F filings title their infotable the same way. This assumption might not hold
-    # we'll deal with that in a bit
+    # Setup #
     
-    # let's manually set the CIK for now
-    # cik.uchicago <- "314957"
-    
-    # Let's start using CIK numbers from the dataset we imported
-    
-    
-    # create object for filing tables directory
-    dir_filing_tables <- paste0(p_dir_in_base, "Filing_Tables")
-    
-    # import dataset
-    filing_tables <- as.vector(list.files(dir_filing_tables, full.names = FALSE))
-    # NVM dont want to do this now
-      # OKAY so I set full.names to FALSE because I want to string split the names so I can identify each university
-      # I can generate full names using dir_filing_tables later
-    
-    # create for loop to read in each filing table
-    # we want the for loop to add each filing table to a list named by university
-    
-    # ls_fltbls <- list()
-    dt_fltbls <- data.table()
-    
-    # names(ls_fltbls) <- filing_tables
-    
-    for(i in 1:length(filing_tables)){
+      # Create filing tables directory string
+      dir_filing_tables <- paste0(p_dir_in_base, "Filing_Tables")
       
-      # extract name of first element
-      current_filename <- as.vector(filing_tables[i])
-      
-      # split current_filename into parts
-      filename_parts <- as.vector(str_split(current_filename, "_"))
-        # isolate each part as a separate vector
-        current_cik <- str_replace(filename_parts[[1]][2], "CIK", "")
-        current_entity_name <- str_replace(filename_parts[[1]][3], ".csv", "")
-      
-      # create full.name filepath for ith filing table
-      current_filepath <- paste0(dir_filing_tables, "/", current_filename)
-      
-      # read csv for current filing table + assign to datatable object
-      current_fltbl <- as.data.table(read.csv(current_filepath))
-      
-      # attach datatable object to corresponding list element
-      # ls_fltbls$names[i] <- dt_fltbl
-      
-      # OKAY for some reason, I am struggling to attach the datatable to a list, so I'll just rbind to a datatable
-        # instead and add the entity name to that datatable as another variable.
-        # should be able to attach the CIK number as well
-      
-      # add metadata to current_fltbl
-      current_fltbl[, c("Entity.Name", "CIK.Number") := .(current_entity_name, current_cik)]
-      
-      # bind current_fltbl to existing dt_fltbls
-      dt_fltbls <- rbind(dt_fltbls, current_fltbl)
-      
-    }
+      # Create vector of filing table csvs in directory
+      filing_tables <- as.vector(list.files(dir_filing_tables, full.names = FALSE))
+      # NVM dont want to do this now
+        # OKAY so I set full.names to FALSE because I want to string split the names so I can identify each university
+        # I can generate full names using dir_filing_tables later
+  
     
-    # # import first element of filing_tables
-    # fltbl.uchicago <- as.data.table(read.csv(filing_tables[1])) # yay this works!
-    # # later, we can automate this process with a for loop
+    # For Loop #
     
-    # EXTRACT ASCCENSION NUMBER + CONSTRUCT FILING URL
+      # Create empty datatable to add to recursively
+      dt_fltbls <- data.table()
+      
+      # Create empty datatable to add cik and entity names to
+      dt_metadata <- data.table(current_cik = character(), current_entity_name = character())
     
-    # before we move onto the next step of extracting the ascension number, we need to remove any filings that aren't
-      # 13F-HR and 13F-HR/A are what we're looking for
+      # create for loop to read in each filing table  
+      for(i in 1:length(filing_tables)){
+        
+        # Extract name of ith csv
+        current_filename <- as.vector(filing_tables[i])
+        
+        # Split current_filename into parts
+        filename_parts <- as.vector(str_split(current_filename, "_"))
+          
+          # isolate each part as a separate vector
+          current_cik <- str_replace(filename_parts[[1]][2], "CIK", "")
+          current_entity_name <- str_replace(filename_parts[[1]][3], ".csv", "")
+          
+          # create vector of metadata
+          current_metadata <- data.table(current_cik, current_entity_name)
+          
+          # add current metadata to dt_metadata
+          dt_metadata <- rbind(dt_metadata, current_metadata)
+        
+        # create full.name filepath for ith filing table
+        current_filepath <- paste0(dir_filing_tables, "/", current_filename)
+        
+        # Read csv for current filing table + assign to datatable object
+        current_fltbl <- as.data.table(read.csv(current_filepath))
+        
+        # Add metadata to current_fltbl
+        current_fltbl[, c("Entity.Name", "CIK.Number") := .(current_entity_name, current_cik)]
+        
+        # bind current_fltbl to existing dt_fltbls
+        dt_fltbls <- rbind(dt_fltbls, current_fltbl)
+        
+      }
     
-    # filter by Form.type
-    # dt_13f <- dt_fltbls[Form.type %like% "13F-HR"]
+    # Post-Processing #
     
       # now we can extract ascension numbers and create urls in the same table
       # since we've created a datatable, we can avoid using a for loop, just need to referentially create a new column
-    
-      # wait I can combine these into one step
-    
-    # create parameter for suffix
-    p_filing_suffix <- "-index.htm"
-    
-    # filter datatagble to Form 13F and create filing URL
-    dt_13f <- dt_fltbls[Form.type %like% "13F-HR", 
-                        Filing.URL := paste(p_url_data, CIK.Number, gsub("-", "", x = Accession.number), paste0(Accession.number, p_filing_suffix), sep = "/")]
+      
+      # create parameter for suffix
+      p_filing_suffix <- "-index.htm"
+      
+      # filter datatable to create filing URL and filter to just 13F reports
+      dt_13f <- dt_fltbls[, Filing.URL := paste(p_url_data, CIK.Number, gsub("-", "", x = Accession.number), paste0(Accession.number, p_filing_suffix), sep = "/")][Form.type %like% "13F-HR"]
     
     # OKAY YAY THIS WORKS!!
-    
-    
-  # TESTING
-    
-    # # extract first ascension number
-    # # filing.1 <- fltbl.uchicago[Film.number.s. == "201240648", Accession.number]
-    # 
-    # # construct complete url to filing page
-    # 
-    # # we're trying to recreate this: 
-    # # https://www.sec.gov/Archives/edgar/data/314957/000110465920115172/0001104659-20-115172-index.htm
-    # # https://www.sec.gov/Archives/edgar/data/314957/000110465920115172/0001104659-20-115172/-index.htm
-    # 
-    # # p_filing_suffix <- "-index.htm"
-    # 
-    # # filing.url <- paste(p_url_data, cik.uchicago, filing.1, p_filing_suffix, sep = "/")
-    # # https://www.sec.gov/Archives/edgar/data/314957/0001104659-20-115172/-index.htm
-    # 
-    # # okay we're missing a collapsed ascension number in between the cik number and the hyphenated asccension number. weird right?
-    # 
-    # # try again
-    # filing.url <- paste(p_url_data, cik.uchicago, gsub("-", "", x = filing.1), paste0(filing.1, p_filing_suffix), sep = "/")
-    # 
-    # # https://www.sec.gov/Archives/edgar/data/314957/000110465920115172/0001104659-20-115172/-index.htm
-    # # okay this looks right! lets run a compare script real quick
-    # identical(filing.url, "https://www.sec.gov/Archives/edgar/data/314957/000110465920115172/0001104659-20-115172-index.htm")
-    # 
-    # # TRUE!
-    # 
-    # # okay lets stop here
-    # # Okay so I should be able to feed the generate url into the next step
 
-# ============================== STAGE TWO: Filing Page ==============================
+# =============================================================================
+# ============================ Infotable Link  =================================
+# =============================================================================
+      
+  # Now we need to obtain the URL for the information table. I noticed that 
+    
+  for (filing.url in dt_13f[, Filing.URL]) {
+    
+    # Set if condition for NA filing URLs
+      # Question: why are there NA filing urls? Need to figure out this problem.
+      
+    if (is.na(filing.url)) {
+      
+      # Add inftbl url to dt_13f
+      dt_13f[Filing.URL == filing.url, Infotable.URL := NA]
+      
+    } else {
+    
+      # Spoof user agent
+      filing.spoof <- GET(filing.url, add_headers('user-agent' = 'SEC-13F-Scraper ([[abhinav.s.krishnan@vanderbilt.edu]])'))
+     
+      # Extract all hyperlinks from webpage
+      filing.html <- filing.spoof %>% 
+        read_html() %>% 
+        html_elements('a') %>% # here, using the html class works better than trying to use the xpath
+        html_attrs()
+      
+        # this code produces a list of attributes for each html element with tag 'a' on the website
+        # the a tag, along with href =, is used to denote hyperlinks
+      
+      # Unlist list of hyperlinks obtained from filing webpage
+      filing.links <- data.table(unlist(filing.html))
+      
+      # filter to hyperlink to Form 13F html infotable
+      inftbl.url.partial <- filing.links[grepl("infotable.xml", V1) & grepl("Form13F", V1)]
+        # NOTE: Here we're assuming that all infotables are marked as such. we may need to do this by reference instead
+      
+      # append base url to partial url
+      inftbl.url <- inftbl.url.partial[, paste0(p_url_base, V1)] # yay this works too!
+      
+      # Add inftbl url to dt_13f
+      dt_13f[Filing.URL == filing.url, Infotable.URL := inftbl.url]
+      
+      # Delay Execution to avoid Too Many Requests issue
+      Sys.sleep(sample(1:2, 1))
+   
+    }
+      
+  }
+    
+    # Here's the issue with this method: I can only identify urls based on their contents.
+    # since the url to the infotable is often named differently, this method is not robust.
+    # Instead, I should try to extract the table on the webpage AND THEN use the "TYPE" column to identify the url I need
+      
+    # UPDATE: I just ran this for loop and it works without errors, but the problem above is still an issue.
+      # We can address this in the next version
+  
+# =============================================================================
+# =============================== Extracting Infotable ====================================
+# =============================================================================
 
-  # set filing url as an object
-  # filing.url <- "https://www.sec.gov/Archives/edgar/data/314957/000110465920115172/0001104659-20-115172-index.htm"
-  
-  # now that we can generate the filing url from the cik number and the ascension numbers, we don't need to manually set it
+    # Appending infotables directly dt_13f will create a HUGE dataset that will be cumbersome to navigate
+    # Instead, I'll try to create a list for each entity and each year.
+      # then, I can unlist and stack each level together
+    # Realistically, the infotable datasets don't need any extra documentation information
+      
+    # Create empty list with slots for every entity
+      ls_entities <- vector(mode = 'list', length = length(dt_metadata[, current_entity_name]))
+      
+      ls_entities <- setNames(ls_entities, dt_metadata[, current_entity_name])
+      
+      # It would also be helpful to create a sublist of years to add each corresponding year's infotables to
+      # For now, however, we'll just add infotables to the entity names
     
+      # Set column names for modification in loop
+        
+        # Old and new column names
+        old_inftbl_names <- paste0("X", seq(1, 12, 1))
+        new_inftbl_names <- c("Name of Issuer", "Title of Class", "CUSIP", "Value", "Shares Amnt", "SH or PRN", "Put or Call", "Investment Discretion", "Other Manager", "Sole Voting Authority", "Shared Voting Authority", "None Voting Authority")
+        
+        # Set Header rows to be removed
+        inftbl_remove_rows <- c("COLUMN 1", "", "NAME OF ISSUER")
+      
+    for (entity in dt_metadata[, current_entity_name]) {
+      
+      # Create empty datatable to store datatbles for each entity
+      dt_entity <- data.table()
+      
+      for (inftbl.url in dt_13f[Entity.Name == entity, Infotable.URL]) {
+        
+        # Set if condition for NA filing URLs
+        # Question: why are there NA filing urls? Need to figure out this problem.
+        
+        if (is.na(inftbl.url) | inftbl.url == p_url_base) {
+          
+          # Add inftbl url to dt_13f
+          dt_13f[Infotable.URL == inftbl.url, Infotable.URL := NA]
+          
+        } else { 
+          
+          # Record the year of the filing
+          current_year <- dt_13f[Infotable.URL == inftbl.url, str_sub(Reporting.date, 1, 4)]
+          
+          # Spoof user agent
+          inftbl.spoof <- GET(inftbl.url, add_headers('user-agent' = 'SEC-13F-Scraper ([[abhinav.s.krishnan@vanderbilt.edu]])'))
+          
+          # Extract entire table of information
+          current.inftbl.dt <- inftbl.spoof %>% 
+            read_html() %>% 
+            html_node(xpath = '/html/body/table[2]') %>% # this seems to be the xml path for all tables
+            html_table()
+          
+          # Set new column names
+          setnames(current.inftbl.dt, old = old_inftbl_names, new = new_inftbl_names)
+          
+          # Remove header rows
+          # current.inftbl.dt[!grepl(pattern = inftbl_remove_rows, x = 'Name of Issuer')]
+          
+          # RBind current infotable datatable to collective infotable datatable
+          dt_entity <- rbind(dt_entity, current.inftbl.dt)
+          
+        }
+        
+      }
+      
+      ls_entities[[entity]] <- dt_entity
+      
+    }  
+      
     
-    
-  # adding user agent to url header to spoof request
-  filing.spoof <- GET(filing.url, add_headers('user-agent' = 'SEC-13F-Scraper ([[abhinav.s.krishnan@vanderbilt.edu]])'))
- 
-  # read html using modified url with headers
-  filing.html <- filing.spoof %>% 
-    read_html() %>% 
-    html_elements('a') %>% # here, using the html class works better than trying to use the xpath
-    html_attrs()
-  
-  # this code produces a list of attributes for each html element with tag 'a' on the website
-    # the a tag, along with href =, is used to denote hyperlinks
-  
-  # unlist list of hyperlinks obtained from filing webpage
-  filing.links <- data.table(unlist(filing.html))
-  
-  # filter to hyperlink to Form 13F html infotable
-  inftbl.url.partial <- filing.links[grepl("infotable.xml", V1) & grepl("Form13F", V1)]
-  # NOTE: Here we're assuming that all infotables are marked as such. we may need to do this by reference instead
-  
-  # now we want to take the partial url from the datatable and add the base url to that. 
-  # lets see if we can do that within the datatable syntax
-  
-  # append base url to partial url
-  inftbl.url <- inftbl.url.partial[, paste0(p_url_base, V1)] # yay this works too!
-  
-  
-  # Now the URL extracted can be appended to the base url to obtain the url to the infotable!!
-  
-  # I should be able to repeat this process to extract information from the company search page!
-  
-# ============================== STAGE TWO: Company Search Page ==============================
+      
+# ============================== WORKSPACE: Company Search Page ==============================
   
   # set filing url as an object
   search.url <- "https://www.sec.gov/edgar/browse/?CIK=314957"
