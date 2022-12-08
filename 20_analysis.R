8679e68eb6f0df8128d9abd0edc3855a1aa4ed59
Abhinav Krishnan <abhinavskrishnan@Abhinavs-MacBook-Pro-2.local>
2022-12-07 22:31:04 -0600

    Created frequency tables of shares to solve cusip to ticker problem

--- 20_analysis.R
+++ 20_analysis.R
@@ -44,23 +44,26 @@
     p_dir_out <- ""
     
     # set input directory
-    p_dir_in_base <- "/Users/abhinavskrishnan/Documents/Projects/SEC-Edgar/Output"
+    p_dir_in_base <- "/Output/"
   
   # ============== #
   # Load Data
   # ============== #
 
     # Create list of infotable datasets
-    vec_datasets <- list.files(p_dir_in_base, full.names = T)
+    vec_datasets <- list.files(path = "Output/", full.names = T)
     
     # Create filepath of most recent file
     # filepath_dt_recent <- paste0(p_dir_in_base_vec_datasets[length(vec_datasets)])
     
     # Read most recent file since length(vec_datasets) returns the index of the most recent dataset
-    in_dt_13f <- read.csv(file = vec_datasets[length(vec_datasets)])
+    in_dt_13f <- as.data.table(read.csv(file = vec_datasets[length(vec_datasets)]))
     
     # Import Fossil Free Funds Shareclass Dataset
-    in_shareclass <- read_xlsx(path = "/Users/abhinavskrishnan/Documents/Projects/SEC-Edgar/FFF-Company-Screens/Invest+Your+Values+shareclass+results+20221109.xlsx", sheet = 2)
+    in_shareclass <- read_xlsx(path = "FFF-Company-Screens/Invest+Your+Values+shareclass+results+20221109.xlsx", sheet = 2)
+    
+    # Import CUSIP to Ticker dataset
+    in_cusip_ticker <- read_xlsx(path = "snp100_cusip_database.xlsx")
     
 # =============================================================================
 # =============================== Linking Datasets ====================================
@@ -80,4 +83,18 @@
   # This becomes a LOT more challenging with mutual funds that change in composition over time. I may need to download several years of Shareclass Results from Fossil Free Funds in order to figure out how mutual funds have changed over time
     
     # ugh this gets more complicated every day lol.
+    
+  # I could solve this problem by manually looking up CUSIP numbers while I wait for a different dataset
+  # I need to know the most common funds so I can manually look them up
+    
+  # Create frequency table of most frequent issuer and class titles and CUSIP, in descending order of frequency
+  frqtbl_shares <- in_dt_13f[, .N, by = c('Name.of.Issuer', 'Title.of.Class', 'CUSIP')][order(-N)]
+    
+  # Create frequency table of most frequent CUSIP numbers
+  # frqtbl_cusip <- in_dt_13f[, .N, by = CUSIP][order(-N)]
+  
+  head(in_dt_13f[, .N, by = CUSIP][order(-N)])
+  
+  # quick visualization for funzies
+  barplot(frqtbl_shares$N, names.arg = frqtbl_shares$Name.of.Issuer)
     
