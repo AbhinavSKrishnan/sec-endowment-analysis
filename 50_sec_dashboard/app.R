6ca09e325ef12dfa467c175befebf15dcf5feb92
Abhinav Krishnan <abhinavskrishnan@Abhinavs-MacBook-Pro-2.local>
2022-12-24 16:16:20 +0530

    minor changes

--- 50_sec_dashboard/app.R
+++ 50_sec_dashboard/app.R
@@ -1,15 +1,55 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#############################################
+# Title: 50_shiny_dashboard
+# Author: Abhinav Krishnan
+# Date: 07 December 2022
+# Inputs: in_dt_13f
+# Outputs: 
+#############################################
+
+# =============================================================================
+# =============================== Description =================================
+# =============================================================================
+
+    # OKay so this dashboard needs to do the following:
+        # Display the datatable
+        # illustrate the purchases made by each entity
+        # illustrate change in purchases over time by entity and 
+
+# =============================================================================
+# ================================= Setup =====================================
+# =============================================================================
+
+# ============== #
+# Load Libraries
+# ============== #
+
+library(data.table)
+library(ggplot2)
+library(stringr)
 
 library(shiny)
+    
+# ============== #
+# Load Data
+# ============== #
+
+in_dt_13f <- readRDS(file = "dt_13f")
+readRDS(file = "frqtbl_shares")
 
 entity_names <- unique(in_dt_13f[, "Entity"])
+
+# =============================================================================
+# =============================== Workspace ===================================
+# =============================================================================
+
+    gg_base <- ggplot(in_dt_13f)
+
+    #gg_base + geom_bar(mapping = aes(x = )
+
+# =============================================================================
+# ================================= UI ======================================
+# =============================================================================
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -32,10 +72,19 @@ ui <- fluidPage(
     )
 )
 
+# =============================================================================
+# =============================== Server =====================================
+# =============================================================================
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
     output$tableEntities <- renderTable(expr = in_dt_13f[])
+    
+    
+    
+    
 }
 
 # Run the application 
